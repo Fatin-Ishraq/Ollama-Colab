56709a6cb24fe5586cd4c09efc8c3e05e511977d
Md.Fatin Ishraq <152465554+Fatin-Ishraq@users.noreply.github.com>
2024-04-28 17:42:20 -0700

    Create ollamaai.py

---
+++ ollamaai.py
@@ -0,0 +1,27 @@
+import os
+import asyncio
+# Set LD_LIBRARY_PATH so the system NVIDIA library
+os.environ.update({'LD_LIBRARY_PATH': '/usr/lib64-nvidia'})
+async def run_process(cmd):
+  print('>>> starting', *cmd)
+  p = await asyncio.subprocess.create_subprocess_exec(
+      *cmd,
+      stdout=asyncio.subprocess.PIPE,
+      stderr=asyncio.subprocess.PIPE,
+  )
+  async def pipe(lines):
+    async for line in lines:
+      print(line.strip().decode('utf-8'))
+
+  await asyncio.gather(
+      pipe(p.stdout),
+      pipe(p.stderr),
+  )
+#register an account at ngrok.com and create an authtoken and place it here
+await asyncio.gather(
+    run_process(['ngrok', 'config', 'add-authtoken','2cdOxw4tEaEOUu8k4ZSqLBGdA90_5849ZpdMcucwxUVxQELAY'])
+)
+await asyncio.gather(
+    run_process(['ollama', 'serve']),
+    run_process(['ngrok', 'http', '--log', 'stderr', '11434']),
+)
